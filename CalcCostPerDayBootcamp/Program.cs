99f3f7bcf94a2d1a17fbfff29915f46f8a5c1b12
Dan Storey <danstorey901@gmail.com>
2021-09-24 09:57:32 -0400

    Add project files.

---
+++ CalcCostPerDayBootcamp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            // My Solution
+            int Cost = 15500;
+            int Duration = 65;
+            Console.WriteLine($"The Bootcamp is {Duration} days long and costs ${Cost / Duration} per day.");
+
+            
+            /* Greg's Solution
+
+            var CostOfBootcamp = 15500;
+            var NumberofDays = 65;
+            var PricePerDay = CostOfBootcamp / NumberofDays;
+            Console.WriteLine($"The Bootcamp costs ${PricePerDay} per day");
+
+            */
+            //Greg's Enhancements - easier to round a single variable! scalable in business solutions
+
+            var CostOfBootcamp = 15500m;
+            var NumberofDays = 65;
+            var PricePerDay = CostOfBootcamp / NumberofDays;
+            Console.WriteLine($"The Bootcamp costs ${PricePerDay} per day");
+
+            // Parth's Solution
+            /*
+            int Cost = 15500;
+            int Duration = 65
+            Console.WriteLine($"The Bootcamp is {Duration} days long and costs ${Cost / Duration} per day.");
+            */
+        }
+    }
+}
+/* 65 days in bootcamp, $15500 total, new project, calculate how much it costs per day = 238.46*/
+
+
+
+
+
+
+
